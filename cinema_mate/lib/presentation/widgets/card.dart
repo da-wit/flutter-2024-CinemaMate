91d1e785fe6a0ffb4f5d819bfd45e3944087d5d1
Dawit Belay <dawitbelay0707@gmail.com>
2024-04-15 21:15:57 +0300

    AppCard modified

--- cinema_mate/lib/presentation/widgets/card.dart
+++ cinema_mate/lib/presentation/widgets/card.dart
@@ -6,22 +6,24 @@ class AppCard extends StatelessWidget {
       {super.key,
       required this.title,
       required this.imgpath,
-      this.width = 175});
+      this.height = 300,
+      this.top = 120});
   final String title;
   final String imgpath;
-  final double? width;
+  final double? height;
+  final double? top;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.elliptical(20, 20))),
+          borderRadius: BorderRadius.all(Radius.elliptical(10, 10))),
       clipBehavior: Clip.hardEdge,
       child: Stack(
         children: [
           SizedBox(
-            width: width,
-            height: 240,
+            // width: width,
+            height: height,
             // height: double.infinity,
             child: FadeInImage(
               placeholder: MemoryImage(kTransparentImage),
@@ -30,10 +32,10 @@ class AppCard extends StatelessWidget {
             ),
           ),
           Positioned(
-            left: 20,
-            right: 20,
-            top: 170,
-            bottom: 10,
+            left: 10,
+            right: 10,
+            top: top,
+            bottom: 5,
             child: Container(
               decoration: const BoxDecoration(
                   color: Color.fromARGB(139, 0, 0, 0),
@@ -42,6 +44,7 @@ class AppCard extends StatelessWidget {
                 child: Text(
                   title,
                   style: const TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
                 ),
               ),
             ),
