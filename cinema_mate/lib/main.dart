71d1bf793636d37d740f97ef1db904d369018c9b
Johna210 <solomonjohna21@gmail.com>
2024-04-15 10:40:02 +0300

    Added color file'

--- cinema_mate/lib/main.dart
+++ cinema_mate/lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+import 'package:cinema_mate/presentation/widgets/components.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World'),
+        ),
+        body: Center(
+          child: AppButton(
+            title: 'Hello',
+            width: 50,
+            onPressed: () {},
+          ),
+        ),
+      ),
+    ),
+  );
+}
