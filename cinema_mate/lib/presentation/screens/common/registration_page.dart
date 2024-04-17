0130fefda8e7dee55f3f28dcacaa7f0be686fd20
Dawit Belay <dawitbelay0707@gmail.com>
2024-04-17 16:06:55 +0300

    Registration page added

---
+++ cinema_mate/lib/presentation/screens/common/registration_page.dart
@@ -0,0 +1,94 @@
+import 'package:cinema_mate/presentation/widgets/buttons.dart';
+import 'package:cinema_mate/presentation/widgets/register.dart';
+import 'package:flutter/material.dart';
+
+class Registration extends StatefulWidget {
+  const Registration({super.key});
+
+  @override
+  State<Registration> createState() {
+    return _RegistrationState();
+  }
+}
+
+class _RegistrationState extends State<Registration> {
+  var formNumber = 1;
+  // var pass = 1;
+
+  Widget _selected = const UserRegistration();
+
+  void ChangeForm(i) {
+    setState(
+      () {
+        if (i == 1) {
+          _selected = const UserRegistration();
+          formNumber = i;
+          // pass = 1;
+        }
+        if (i == 2) {
+          _selected = const CinemaRegistration();
+          formNumber = i;
+          // pass = 2;
+        }
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.black,
+              Colors.red,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 100, bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    AppButton(
+                        title: "User",
+                        width: 180,
+                        onPressed: () {
+                          ChangeForm(1);
+                        }),
+                    AppButton(
+                        title: "Cinema name",
+                        width: 180,
+                        onPressed: () {
+                          ChangeForm(2);
+                        })
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              _selected,
+              AppButton(
+                title: "Sign Up",
+                width: 250,
+                onPressed: () {
+                  //use the formnumber to pass values to the login page
+                },
+                textSize: 30,
+                height: 70,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
