30978186c3f30ebb0435a500b6ba0fda4a923f22
Johna210 <solomonjohna21@gmail.com>
2024-04-19 10:02:34 +0300

    Modifed main

--- cinema_mate/lib/main.dart
+++ cinema_mate/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cinema_mate/presentation/screens/common/registration_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cinema_mate/presentation/screens/user/user_tab_bar.dart';
@@ -7,7 +6,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Center(
-        child: Registration(),
+        child: UserTabBar(),
       ),
     ),
   );
